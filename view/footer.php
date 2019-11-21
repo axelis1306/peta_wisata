02559eefd430a73783fd6f8e51e81fb1cd8e7d8c
fatimahazzahra34 <58028062+fatimahazzahra34@users.noreply.github.com>
2019-11-21 22:49:13 +0700

    Menambahkan view halaman web

---
+++ view/footer.php
@@ -0,0 +1,85 @@
+
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="footer, address, phone, icons" />
+
+	<title>Footer With Address And Phones</title>
+
+	<link rel="stylesheet" href="css/demo.css">
+	<link rel="stylesheet" href="css/footer-distributed-with-address-and-phones.css">
+	
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+
+	<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+
+</head>
+
+		<footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<h3>Pariwisata<span>Malang</span></h3>
+
+				<p class="footer-links">
+					<a href="index.php">Home</a>
+					·
+
+					<a href="indexExplore.php">Explore</a>
+					·
+					<a href="maps5.php">Digital Maps</a>
+					·
+					<a href="https://budpar.malangkota.go.id/">Web Disbudpar</a>
+					·
+					<!-- <a href="#">Social Media</a>
+					· -->
+					<a href="#video">About</a>
+				<!-- 	·
+					<a href="#">Contact</a> -->
+				</p>
+
+				<p class="footer-company-name">Nonino &copy; 2019</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>Komplek Mpu Purwa</span> Jl. Soekarno Hatta B. 210</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>(0341) 404515</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="#">budpar@malangkota.go.id</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>Dinas Budaya & Pariwisata</span>
+					Kota Malang adalah sebuah kota yang terletak di Provinsi Jawa Timur, Indonesia. Kota ini terletak 90 km sebelah selatan Surabaya dan merupakan kota terbesar di kedua di Jawa Timur.
+				</p>
+
+				<div class="footer-icons">
+
+					<a href="https://www.youtube.com/Malang%20Menyapa"><i class="fa fa-youtube-play"></i></a>
+					<a href="https://www.instagram.com/malang.menyapa"><i class="fa fa-instagram"></i></a>
+					<a href="https://www.facebook.com/disbudparkotamalang/"><i class="fa fa-facebook-official"></i></a>
+					<a href="https://twitter.com/BUDPARMALANG"><i class="fa fa-twitter"></i></a>
+
+				</div>
+
+			</div>
+
+		</footer>
+
